570df9024385c72695a2220980ce7d9cb6109f0a
Anton Goncharov <42060678+AntonGoncharov1999@users.noreply.github.com>
2023-03-27 23:41:07 +0300

    начал верстать корзину
    начал верстать корзину

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 function App() {
   return (
     <div className="wrapper">
+      <div className='overlay'>
+        <div className='driwer'>
+          <h2>Корзина</h2>
+          <div className='cartItem'>
+            <img width={70} height={80} src='/img/manga/tokyo _evengers_1.webp' alt='' />
+            <div>
+              <p>Токийские мстители. Том 1</p>
+              <b>1 119 руб</b>
+            </div>
+            <img className='removeBtn' width={30} height={30} src='/img/close.svg' alt=''></img>
+          </div>
+        </div>
+      </div>
       <header>
         <div className='headerLeft'>
           <img width={40} height={40} src="/img/logo.png" />
@@ -20,18 +33,68 @@ function App() {
         </div>
       </header>
       <div className='content'>
-        <h1>Манга</h1>
-        <div className='card'>
-          <img width={133} height={157} src="/img//manga/tokyo _evengers_1.webp" />
-          <p>Токийские мстители. Том 1.</p>
-          <div className='cardBottom'>
-            <div className='cardBottomTop'>
-              <span>Цена:</span>
-              <b>1 119 руб.</b>
+        <div className='content-top'>
+          <h1>Манга</h1>
+          <div className='searth-block'>
+            <img width={18} height={18} src="/img/loop.svg" alt="searth" />
+            <input placeholder='Поиск...' />
+          </div>
+        </div>
+        <div className='manga'>
+          <div className='card'>
+            <div className='favorit'>
+              <img height={18} width={18} src="/img/bookmark.svg" alt='unlike' />
             </div>
-            <button>
-              <img width={11} height={11} src="/img/plus.svg" />
-            </button>
+            <img width={133} height={157} src="/img//manga/tokyo _evengers_1.webp" />
+            <h5>Токийские мстители. Том 1.</h5>
+            <div className='cardBottom'>
+              <div className='cardBottomTop'>
+                <span>Цена:</span>
+                <b>1 119 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src="/img/plus.svg" />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={157} src="/img//manga/tokyo _evengers_2.webp" />
+            <h5>Токийские мстители. Том 2.</h5>
+            <div className='cardBottom'>
+              <div className='cardBottomTop'>
+                <span>Цена:</span>
+                <b>1 119 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src="/img/plus.svg" />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={157} src="/img//manga/tokyo _evengers_3.webp" />
+            <h5>Токийские мстители. Том 3.</h5>
+            <div className='cardBottom'>
+              <div className='cardBottomTop'>
+                <span>Цена:</span>
+                <b>1 119 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src="/img/plus.svg" />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={157} src="/img//manga/tokyo _evengers_4.webp" />
+            <h5>Токийские мстители. Том 4.</h5>
+            <div className='cardBottom'>
+              <div className='cardBottomTop'>
+                <span>Цена:</span>
+                <b>1 119 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src="/img/plus.svg" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
